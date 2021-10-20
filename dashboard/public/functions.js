08a1371b6156e35844a7579f9696397a8e31c367
pioupia <49518790+pioupia@users.noreply.github.com>
2021-10-20 13:39:44 +0200

    Add files via upload

--- dashboard/public/functions.js
+++ dashboard/public/functions.js
@@ -26,3 +26,33 @@ function getCookie(name) {
   ));
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
+=======
+const getFrame = (video) => {
+    const canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    const data = canvas.toDataURL('image/png');
+    return data;
+}
+
+const getAudio = (video) => {
+    var ctx = new AudioContext();
+      // create an source node from the <video>
+      var source = ctx.createMediaElementSource(video);
+      // now a MediaStream destination node
+      var stream_dest = ctx.createMediaStreamDestination();
+      // connect the source to the MediaStream
+      source.connect(stream_dest);
+
+      var tampon = new ArrayBuffer(stream_dest.stream);
+      return tampon;
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+>>>>>>> f044f464a3e4cb8da078f39e59f54e086ab918d3
